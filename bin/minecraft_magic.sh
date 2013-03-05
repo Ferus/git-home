b96cc9e5a4184c56a1ce432244eec4db246df311
Ferus <ferus+git@pyboard.net>
2013-03-04 23:16:05 -0800

    .bin/ != bin/

---
+++ bin/minecraft_magic.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+LD_LIBRARY_PATH="/usr/lib/jvm/java-7-openjdk/jre/lib/amd64" java -jar /home/ferus/Games/Minecraft/MagicLauncher.jar
---
+++ bin/notpacman.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+love07 /home/ferus/Games/NotPacman/not_pacman.love
